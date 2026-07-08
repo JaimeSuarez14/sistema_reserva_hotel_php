c6fecd93f2675b27dd76680838db848c8bff4150
Jaime Suarez <jaisua8@gmail.com>
2026-07-07 20:23:43 -0500

    Configuracion de las rutas y sus parametros

---
+++ controllers/principal/Blog.php
@@ -0,0 +1,13 @@
+<?php
+class Blog
+{
+  function index()
+  {
+    echo 'Mensaje desde el controlador';
+  }
+
+   function listar()
+  {
+    echo 'Lista de blogs';
+  }
+}
--- index.php
+++ index.php
@@ -10,15 +10,51 @@ $currentPageUrl = explode('?', $currentPageUrl)[0];
 // Si está vacío, usar ruta por defecto
 $ruta = empty($currentPageUrl) ? 'principal/index' : $currentPageUrl;
 $array = explode('/', $ruta);
+$isAdmin = isset($array[1]) && $array[1] === ADMIN;
+
+// /reservas/admin/blog/crear/parametro1/parametro2
+// /reservas/blog/ver/parametro
 
 // Caso especial: /admin
-if ($array[0] === ADMIN) {
-    $controlador = "Admin";
-    $metodo = isset($array[1]) ? $array[1] : "login";
+if ($isAdmin) {
+
+  $controlador = ucfirst($array[2] ?? 'Admin');
+  $metodo      = $array[3] ?? 'index';
+  $parametros  = array_slice($array, 4);
 } else {
-    $controlador = ucfirst($array[0]);
-    $metodo = isset($array[1]) ? $array[1] : "index";
+
+  $controlador = ucfirst($array[1] ?? 'Principal');
+  $metodo      = $array[2] ?? 'index';
+  $parametros  = array_slice($array, 3);
 }
 
 echo "controller : " . $controlador . "<br>";
 echo "metodo : " . $metodo;
+echo  "<br>";
+
+//validar metodos
+$parametro = '';
+$parametroIndice = ($isAdmin) ? 4 : 3;
+if (!empty($array[$parametroIndice]) && $array[$parametroIndice] != '') {
+  for ($i = $parametroIndice; $i < count($array); $i++) {
+    $parametro .= $array[$i] . ",";
+  }
+  $parametro = trim($parametro, ",");
+  echo $parametro;
+}
+
+
+
+$dirController = ($isAdmin) ? 'controllers/admin/' . $controlador . ".php" : 'controllers/principal/' . $controlador . ".php";
+if (file_exists($dirController)) {
+
+  require_once $dirController;
+  $controller = new $controlador();
+  if(method_exists($controlador, $metodo)){
+    $controller->$metodo($parametro);
+  }else{
+    echo "No existe el metodo";
+  }
+}else{
+  echo "No existe el controlador";
+}
